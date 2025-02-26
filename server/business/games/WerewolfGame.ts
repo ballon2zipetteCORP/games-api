1385840ef88794e6f38c5cd85119c744066707d5
KazouDev <frenchkazou@gmail.com>
2025-02-26 15:36:43 +0100

    add roles settings

--- server/business/games/WerewolfGame.ts
+++ server/business/games/WerewolfGame.ts
@@ -1,61 +1,63 @@
 import BaseGame, { IBaseGameParams } from "./BaseGame";
 
 interface IRole {
-    id: string;
-    displayName: string
-    image: string
-    description: string
-    expectedActions: [string] 
+  id: string;
+  displayName: string;
+  image: string;
+  description: string;
+  expectedActions: [string];
 }
 
 interface IWerewolfMetadata {
-    roles: IRole[]
+  roles: IRole[];
 }
 
 interface IWerewolfSettings {
-    NUMBER_OF_PLAYER_PER_ROLE: Record<string, number>;
+  NUMBER_OF_PLAYER_PER_ROLE: Record<string, number>;
 }
 
 export default class WerewolfGame extends BaseGame {
+  public constructor({ party, game }: IBaseGameParams) {
+    super({ party, game });
+  }
 
-    public constructor({ party, game }: IBaseGameParams) {
-        super({ party, game });
+  public end(): void {
+    super.end();
+  }
+
+  public start(): void {
+    super.start();
+    this.distributeRoles();
+  }
+
+  private distributeRoles(): void {
+    const players = this.party.players;
+    const settings = this.party.settings as IWerewolfSettings;
+    const metadata = this.game.metadata as IWerewolfMetadata;
+
+    const rolesSettings = Object.entries(
+      settings?.NUMBER_OF_PLAYER_PER_ROLE ??
+        Object.fromEntries(metadata.roles.map(({ id }) => [id, 0]))
+    ).filter((e) => e[1] > 0);
+
+    console.log("settings : ", rolesSettings);
+
+    for (const player of players) {
+      const index =
+        Math.floor(Math.random() * rolesSettings.length) % rolesSettings.length;
+      const [id, quota] = rolesSettings[index];
+      const role = metadata.roles.find((r) => r.id === id)!;
+
+      this.sendMessage<{ role: IRole }>(player.id, {
+        type: "ROLE_GIVEN",
+        data: { role },
+      });
+
+      const newQuota = quota - 1;
+      rolesSettings[index][1] = newQuota;
+      if (newQuota < 1) {
+        rolesSettings.splice(index, 1);
+      }
     }
-
-    public end(): void {
-        super.end();
-    }
-
-    public start(): void {
-        super.start();
-        this.distributeRoles();
-    }
-
-    private distributeRoles(): void {
-        const players = this.party.players;
-        const settings = this.party.settings as IWerewolfSettings;
-        const metadata = this.game.metadata as IWerewolfMetadata;
-
-        const rolesSettings = Object.entries(
-            settings?.NUMBER_OF_PLAYER_PER_ROLE ?? 
-                Object.fromEntries(metadata.roles.map(({id}) => [id, 0]))
-        );
-        for(const player of players) {
-            const index = Math.floor(Math.random() * rolesSettings.length) % rolesSettings.length;
-            const [id, quota] = rolesSettings[index];
-            const role = metadata.roles.find(r => r.id === id)!;
-
-            this.sendMessage<{ role: IRole; }>(player.id, { 
-                type: "ROLE_GIVEN", 
-                data: { role }
-            });
-            
-            const newQuota = quota-1;
-            rolesSettings[index][1] = newQuota;
-            if(newQuota < 1) {
-                rolesSettings.splice(index, 1);
-            }
-        }
-    }
-
+  }
 }
--- server/datasources/Parties.ts
+++ server/datasources/Parties.ts
@@ -1,123 +1,131 @@
-import client from "../services/redis"
-import { IMe } from "../types";
 import gameCache from "../business/games/gameCache";
-import { models } from "../models/index";
 import WerewolfGame from "../business/games/WerewolfGame";
+import { models } from "../models/index";
+import client from "../services/redis";
+import { IMe } from "../types";
 
 export interface IPartyPlayer {
-    id: string;
-    displayName: string;
+  id: string;
+  displayName: string;
 }
 
 export interface IParty {
-    id: string;
-    ownerId: string;
-    players: IPartyPlayer[];
-    gameId: string;
-    settings: unknown;
-    createdAt: Date
+  id: string;
+  ownerId: string;
+  players: IPartyPlayer[];
+  gameId: string;
+  settings: unknown;
+  createdAt: Date;
 }
 
 const GameMapper: any = {
-    werewolf: WerewolfGame
-}
+  werewolf: WerewolfGame,
+};
 
 export default class Parties {
+  readonly TAG = "parties:";
 
-    readonly TAG = "parties:";
+  public async get(id: string): Promise<IParty> {
+    const party = await client.get(this.TAG + id);
+    return party ? JSON.parse(party) : null;
+  }
 
-    public async get(id: string): Promise<IParty> {
-        const party = await client.get(this.TAG+id);
-        return party ? JSON.parse(party) : null;
+  public async create(gameId: string, me: IMe) {
+    const player: IPartyPlayer = me;
+    const game = await models.game.findOne({ id: gameId });
+
+    const GameClass = GameMapper[gameId];
+    const partyId = this.generatePartyID();
+
+    const party = await this.set({
+      id: partyId,
+      gameId,
+      ownerId: player.id,
+      players: [player],
+      settings: {},
+      createdAt: new Date(),
+    });
+
+    // store game instance in an object
+    gameCache.store(partyId, new GameClass({ party, game }));
+
+    return party;
+  }
+
+  public async leave(id: string, playerId: string) {
+    const party = await this.get(id);
+    const players = [...party.players];
+    let ownerId = party.ownerId;
+
+    // remove the player from the list
+    const player = players.find((p) => p.id === playerId);
+    players.splice(players.indexOf(player!), 1);
+
+    // special case for owner
+    if (ownerId === playerId && players.length) {
+      // change the owner with the first player
+      ownerId = players[0].id;
     }
 
-    public async create(gameId: string, me: IMe) {
-        const player: IPartyPlayer = me;
-        const game = await models.game.findOne({ id: gameId });
+    await this.update(id, { players });
+    const cachedGame = gameCache.retrieve(id);
 
-        const GameClass = GameMapper[gameId];
-        const partyId = this.generatePartyID();
-        
-        const party = await this.set({
-            id: partyId,
-            gameId,
-            ownerId: player.id,
-            players: [player],
-            settings: {},
-            createdAt: new Date()
-        });
+    cachedGame.broadcast<{ player: IPartyPlayer }>({
+      type: "PLAYER_LEAVED",
+      data: { player: player! },
+    });
 
-        // store game instance in an object
-        gameCache.store(partyId, new GameClass({ party, game }));
+    return this.update(id, { ownerId, players });
+  }
 
-        return party;
+  public async saveSettings(id: string, settings: Record<string, unknown>) {
+    return this.update(id, { settings });
+  }
+
+  public async join(id: string, me: IMe) {
+    const party = await this.get(id);
+    if (!party) throw new Error("Party not found.");
+    const players = [...party.players];
+    const player = { id: me.id, displayName: me.displayName };
+
+    players.push(player);
+
+    const updatedParty = await this.update(id, { players });
+    const cachedGame = gameCache.retrieve(id);
+
+    cachedGame.broadcast<{ player: IPartyPlayer }>({
+      type: "PLAYER_JOINED",
+      data: { player },
+    });
+    return updatedParty;
+  }
+
+  private async update(id: string, props: Omit<Partial<IParty>, "id">) {
+    const party = await this.get(id);
+    if (!party) {
+      throw new Error("Party not found");
     }
+    const updatedParty = await this.set({ ...party, ...props });
+    const cachedGame = gameCache.retrieve(id);
 
-    public async leave(id: string, playerId: string) {
-        const party = await this.get(id);
-        const players = [...party.players];
-        let ownerId = party.ownerId;
+    cachedGame?.update(updatedParty as IParty);
+    return updatedParty;
+  }
 
-        // remove the player from the list
-        const player = players.find(p => p.id === playerId);
-        players.splice(players.indexOf(player!), 1);
-        
-        // special case for owner
-        if(ownerId === playerId && players.length) {
-            // change the owner with the first player
-            ownerId = players[0].id;
-        }
+  private async set(party: Partial<IParty>) {
+    await client.set(this.TAG + party.id, JSON.stringify(party), {
+      EX: 60 * 60 * 24,
+    });
+    return party;
+  }
 
-        const updatedParty = await this.update(id, { players });
-        const cachedGame = gameCache.retrieve(id);
-        
-        cachedGame.broadcast<{ player: IPartyPlayer }>({ type: "PLAYER_LEAVED", data: { player: player! } })
-        cachedGame?.update(updatedParty as IParty);
+  private generatePartyID() {
+    const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+    let result = "";
 
-        return this.update(id, { ownerId, players });
+    for (let i = 0; i < 6; i++) {
+      result += letters[Math.floor(Math.random() * letters.length)];
     }
-
-    public async saveSettings(id: string, settings: Record<string, unknown>) {
-        return this.update(id, { settings });
-    }
-
-    public async join(id: string, me: IMe) {
-        const party = await this.get(id);
-        if(!party) throw new Error("Party not found.");
-        const players = [...party.players];
-        const player = {  id: me.id,  displayName: me.displayName  };
-        
-        players.push(player);
-
-        const updatedParty = await this.update(id, { players });
-        const cachedGame = gameCache.retrieve(id);
-        
-        cachedGame.broadcast<{ player: IPartyPlayer }>({ type: "PLAYER_JOINED", data: { player } })
-        cachedGame?.update(updatedParty as IParty);
-        return updatedParty;
-    }
-
-    private async update(id: string, props: Omit<Partial<IParty>, "id">) {
-        const party = await this.get(id);
-        if(!party) {
-            throw new Error("Party not found");
-        }
-        return this.set({ ...party, ...props });
-    }
-
-    private async set(party: Partial<IParty>) {
-        await client.set(this.TAG+party.id, JSON.stringify(party), { EX: 60*60*24 });
-        return party;
-    }
-
-    private generatePartyID() {
-        const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
-        let result = "";
-    
-        for (let i = 0; i < 6; i++) {
-          result += letters[Math.floor(Math.random() * letters.length)];
-        }
-        return result;    
-    }
-
+    return result;
+  }
 }
