3317ccf1f46a4a76b6a5e77f802ac2a9a5a6b714
KazouDev <frenchkazou@gmail.com>
2025-02-26 13:49:03 +0100

    update image

--- mongo-init.js
+++ mongo-init.js
@@ -9,7 +9,8 @@ db.games.insertOne({
       {
         id: "dj",
         displayName: "DJ",
-        image: "https://...",
+        image:
+          "https://s3.matteogaillard.fr/api/v1/buckets/zipette-games/objects/download?preview=true&prefix=dj.png&version_id=null",
         description:
           "Doit faire passer 3 musiques nulles choisies par l'application.",
         expectedActions: ["Jouer des musiques de mauvaise qualité"],
@@ -17,21 +18,24 @@ db.games.insertOne({
       {
         id: "villageois",
         displayName: "Villageois",
-        image: "https://...",
+        image:
+          "https://s3.matteogaillard.fr/api/v1/buckets/zipette-games/objects/download?preview=true&prefix=villageois.png&version_id=null",
         description: "Fait sa vie franchement.",
         expectedActions: ["Profiter de la soirée sans rôle particulier"],
       },
       {
         id: "idiot_du_village",
         displayName: "L'Idiot du village",
-        image: "https://...",
+        image:
+          "https://s3.matteogaillard.fr/api/v1/buckets/zipette-games/objects/download?preview=true&prefix=idiot_du_village.png&version_id=null",
         description: "Doit dire le même mot souvent.",
         expectedActions: ["Répéter un mot spécifique toute la soirée"],
       },
       {
         id: "saboteur",
         displayName: "Le Saboteur",
-        image: "https://...",
+        image:
+          "https://s3.matteogaillard.fr/api/v1/buckets/zipette-games/objects/download?preview=true&prefix=saboteur.png&version_id=null",
         description: "Sabote les verres des autres.",
         expectedActions: ["Ajouter des ingrédients inattendus dans les verres"],
       },
@@ -39,14 +43,15 @@ db.games.insertOne({
         id: "barman",
         displayName: "Le Barman",
         image:
-          "http://46.202.168.50:9001/api/v1/buckets/zipette-games/objects/download?preview=true&prefix=bartender.png&version_id=null",
+          "https://s3.matteogaillard.fr/api/v1/buckets/zipette-games/objects/download?preview=true&prefix=barman.png&version_id=null",
         description: "Propose des mélanges douteux.",
         expectedActions: ["Préparer des cocktails étranges et inhabituels"],
       },
       {
         id: "amoureux",
         displayName: "Les Amoureux",
-        image: "https://...",
+        image:
+          "https://s3.matteogaillard.fr/api/v1/buckets/zipette-games/objects/download?preview=true&prefix=amoureux.png&version_id=null",
         description:
           "Deux joueurs choisis par l'application doivent avoir le même verre, même quantité et être dans la même pièce.",
         expectedActions: [
@@ -56,7 +61,8 @@ db.games.insertOne({
       {
         id: "copieur",
         displayName: "Le Copieur",
-        image: "https://...",
+        image:
+          "https://s3.matteogaillard.fr/api/v1/buckets/zipette-games/objects/download?preview=true&prefix=copieur.png&version_id=null",
         description:
           "Désigne une personne et doit boire à chaque fois que cette personne boit.",
         expectedActions: ["Imiter la consommation d’un joueur désigné"],
@@ -64,7 +70,8 @@ db.games.insertOne({
       {
         id: "dictateur",
         displayName: "Le Dictateur",
-        image: "https://...",
+        image:
+          "https://s3.matteogaillard.fr/api/v1/buckets/zipette-games/objects/download?preview=true&prefix=dictateur.png&version_id=null",
         description: "Impose un shot à une personne toutes les heures.",
         expectedActions: [
           "Désigner une personne pour boire un shot chaque heure",
@@ -74,7 +81,7 @@ db.games.insertOne({
         id: "serpillere",
         displayName: "La Serpillière",
         image:
-          "https://png.pngtree.com/png-clipart/20240313/original/pngtree-bartender-or-barman-character-png-image_14581474.png",
+          "https://s3.matteogaillard.fr/api/v1/buckets/zipette-games/objects/download?preview=true&prefix=serpillere.png&version_id=null",
         description:
           "Bien joué, tu t’es fait avoir ! Tu devras venir nettoyer demain.",
         expectedActions: ["Nettoyer après la soirée"],
@@ -82,14 +89,16 @@ db.games.insertOne({
       {
         id: "arabe",
         displayName: "L'Arabe",
-        image: "https://...",
+        image:
+          "https://s3.matteogaillard.fr/api/v1/buckets/zipette-games/objects/download?preview=true&prefix=arabe.png&version_id=null",
         description: "Essaye de voler le verre des autres.",
         expectedActions: ["Prendre discrètement le verre des autres joueurs"],
       },
       {
         id: "pyromane",
         displayName: "Le Pyromane",
-        image: "https://...",
+        image:
+          "https://s3.matteogaillard.fr/api/v1/buckets/zipette-games/objects/download?preview=true&prefix=pyromane.png&version_id=null",
         description:
           "Doit mettre de la sauce piquante dans les verres des autres.",
         expectedActions: ["Ajouter de la sauce piquante dans les boissons"],
